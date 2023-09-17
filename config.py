362723ecec55e92efb393c079ec86ebcd737acaa
Jing <83607311+Jing-Rou@users.noreply.github.com>
2023-09-18 07:49:34 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "student.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custompass = "abcd1234"
+customdb = "itps"
+custombucket = "ongjingrou-itps"
 customregion = "us-east-1"
 

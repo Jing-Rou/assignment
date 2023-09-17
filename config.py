4eac0aaa3f7af67c68ff040b8b62552d341bb165
Jing <83607311+Jing-Rou@users.noreply.github.com>
2023-09-18 07:51:48 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "student.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "database-2.cyltbqip9evc.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "abcd1234"
 customdb = "itps"

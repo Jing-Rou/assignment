50e30cbe0d22d35dacd7e79181a0be94da969440
Jing <ongjingrou090@gmail.com>
2023-09-18 14:19:46 +0800

    Your commit message

--- app.py
+++ app.py
@@ -90,13 +90,3 @@ def login():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
 
-n.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-
-n.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
-

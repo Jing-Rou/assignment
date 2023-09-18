d3adfcc88cdfe3fe2f24ab53a445ca8aaf392b83
Jing <ongjingrou090@gmail.com>
2023-09-18 10:34:31 +0800

    Your commit message

--- test_run.py
+++ test_run.py
@@ -38,11 +38,11 @@ def job_details():
 def contact():
     return render_template('contact.html')
 
-@app.route("/register", methods=['POST'])
+@app.route("/register", methods=['GET'])
 def register():
     return render_template('register.html')
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=['GET'])
 def login():
     return render_template('login.html')
 

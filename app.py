1dc15cbef86b48204b2d5c06c6274be7f44b3cf1
Jing <ongjingrou090@gmail.com>
2023-09-22 20:21:36 +0800

    Your commit message

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ def studentProfile():
         cursor.execute(select_sql, (student_id))
         data = cursor.fetchall()  # Fetch a single row
         data = data[0]
-        data = str(data[12])[:10]
+        data[12] = str(data[12])[:10]
 
     except Exception as e:
         return str(e)
